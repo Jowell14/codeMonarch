a3a0039b91ded4c0d49a7aff10b194cc77651ba9
Joel Adie <adiejoel14@gmail.com>
2021-03-29 04:32:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,3 @@ module.exports = {
         deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     },
 };
-// const dev = process.env.NODE_ENV !== "production";
-
-// export const server = dev ?
-//     "http://localhost:3000" :
-//     "https: //codemonarch-blog.herokuapp.com";
